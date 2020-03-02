88a27ee4a370df93ef1a959910d8f0beaf861120
sebasrodri237 <sebasplay2@hotmail.com>
2020-03-01 22:45:40 -0500

    Agregando javascript de bootstrap y propio al proyecto e incorporando imagenes, en este caso el logo del header

--- Platzigifts/wp-content/themes/platzigifts/functions.php
+++ Platzigifts/wp-content/themes/platzigifts/functions.php
@@ -12,16 +12,27 @@ function init_template(){
 add_action( 'after_setup_theme', 'init_template');
 // Cuando alguien ingrese al sitio WP elige el tema y ejecute las opciones
 
-function assets(){
-
-    // Forma de manejo de libreria que se cargaran antes que nuestra hoja de estilos,
+function assets(){// Forma de manejo de librerias en las que se cargaran como dependencias 
+//CSS
+    // Estas dependencias antes que nuestra hoja de estilos pero no las inicializara dentro del navegador
     // en este caso bootstrap y una fuente
     wp_register_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1', 'all');
     //Nombre de la libreria, url, dependencias que recibe, version, lugares en que se cargara
     wp_register_style( 'montserrat', 'https://fonts.googleapis.com/css?family=Montserrat&display=swap', '', '1.0', 'all');
-    // Forma de ejecutar nuestra hoja de estilos
     wp_enqueue_style( 'estilos', get_stylesheet_uri(), array('bootstrap','montserrat'), '1.0', 'all');
+    // Poner en cola para inicializar nuestra hoja de estilos y las libreria cargadas (dependencias)
     //Nombre de la libreria, url(esta funcion traera nuestra hoja de estilos), dependencias que recibe, version, lugares en que se cargara
+
+//Javascript
+    wp_register_script( 'popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', '', '1.16.0', true);
+    //Nombre de la libreria, url, dependencias que recibe, version, se cargara en footer: true en header: false
+    // Poner en cola bootstrap, aunque la web de bootstrap pedia incorporar JQUERY wordpress por defecto ya lo tiene incorporado en sus librerias.
+    wp_enqueue_script( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery', 'popper'), '4.4.1', true);
+    //Nombre de la libreria, url, dependencias que recibe, version, se cargara en footer: true en header: false
+    wp_enqueue_script( 'custom', get_template_directory_uri().'/assets/javascript/custom.js', '', '1.0', true);
+    //Cargar un archivo javascript propio
+    //Nombre, Forma de llamar archivo (en este caso dinamica, es decir, mediante una funcion llamar al archivo desde cualquier url de nuestro template(plantilla)
+    // ,dependencias a cargar, version y true para footer)
 }
 
 add_action( 'wp_enqueue_scripts', 'assets');
--- Platzigifts/wp-content/themes/platzigifts/header.php
+++ Platzigifts/wp-content/themes/platzigifts/header.php
@@ -6,4 +6,15 @@
     <?php wp_head() ?> <!-- Trae todas las dependencias que se cargen en el encabezado -->
 </head>
 <body>
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/logo.png" alt="logo">
+                <!-- Traer una imagen al header -->
+            </div>
+        </div>
+    </div>
+</header>
     
